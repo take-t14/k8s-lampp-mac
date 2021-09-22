bcc249e165b4b7bebef7390d43e60473f4a1c78f
Tadanobu Hatakeyama <hatake_t14@hotmail.com>
2021-09-23 00:04:59 +0900

    ・Docker Desktop→multipass移行

--- multipass-envinit.sh
+++ multipass-envinit.sh
@@ -5,5 +5,7 @@ export DOCKER_HOST=tcp://multipass-host:2376
 # kubectlの向きをmicrok8sの仮想環境へ向ける
 export KUBECONFIG=~/.kube/microk8s.kubeconfig
 # skaffoldの向きをmicrok8sの仮想環境へ向ける
+skaffold config unset insecure-registries
 skaffold config set insecure-registries multipass-host:32000
+skaffold config unset default-repo
 skaffold config set default-repo multipass-host:32000
